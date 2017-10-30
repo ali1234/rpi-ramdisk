a80f27e39bd9eaa54f806fea419ef3bde8cd8d9f
Alistair Buxton <a.j.buxton@gmail.com>
2017-10-30 02:10:51 +0000

    Create workarounds.mk
    This is for the workarounds needed for autotools/libtool/glib code.
    Separate file because the vastly surperior Qt does not need them.

--- packages/toolchain.mk
+++ packages/toolchain.mk
@@ -5,14 +5,3 @@ STAGE := $(BASEDIR)/root
 TOOLCHAIN := $(shell readlink -f ..)/toolchain/bin/arm-linux-gnueabihf-
 
 ARCH_CFLAGS := -pipe -march=armv7-a -marm -mthumb-interwork -mfpu=neon-vfpv4 -mtune=cortex-a7 -mabi=aapcs-linux -mfloat-abi=hard
-
-# pkg-config wrapper setup
-# Also need to export PKG_CONFIG_LIBDIR in each package Makefile.
-export PKG_CONFIG := $(BASEDIR)/../pkg-config
-export PKG_CONFIG_DIR :=
-export PKG_CONFIG_SYSROOT_DIR := $(SYSROOT)
-
-# Set up Qemu for G-I
-# Also need to export XDG_DATA_DIRS in each package Makefile.
-export QEMU_LD_PREFIX := $(SYSROOT)
-export LD_LIBRARY_PATH := $(SYSROOT)/opt/vc/lib
---
+++ packages/workarounds.mk
@@ -0,0 +1,19 @@
+# Workarounds for libtool and gobject badness.
+
+# pkg-config cannot handle sysroots properly, so we need to use a
+# wrapper to adjust any paths it outputs.
+# Also need to export PKG_CONFIG_LIBDIR in each package Makefile.
+export PKG_CONFIG := $(BASEDIR)/../pkg-config
+export PKG_CONFIG_DIR :=
+export PKG_CONFIG_SYSROOT_DIR := $(SYSROOT)
+
+# Set up Qemu for gobject introspection
+# G-I binding generation is done by building a native executable and
+# then running it to see what it exports. We need Qemu for this.
+# Also need to export XDG_DATA_DIRS in each package Makefile.
+export QEMU_LD_PREFIX := $(SYSROOT)
+export LD_LIBRARY_PATH := $(SYSROOT)/opt/vc/lib
+
+# g-ir-scanner ignores CFLAGS so we must put the sysroot into CC to
+# make sure it is used.
+export CC := arm-linux-gnueabihf-gcc --sysroot=$(SYSROOT)
