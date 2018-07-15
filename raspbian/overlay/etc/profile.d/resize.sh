5cae45b58aa7ac7ef743bf498f412396f7eca1ea
Alistair Buxton <a.j.buxton@gmail.com>
2018-07-15 03:51:51 +0100

    Move a thing into the overlay dir instead of generating it

---
+++ raspbian/overlay/etc/profile.d/resize.sh
@@ -0,0 +1,19 @@
+# The following profile fragment makes the terminal
+# rows/cols be correct when logging in over serial
+
+resize() {
+
+  old=\$(stty -g)
+  stty raw -echo min 0 time 5
+
+  printf '\033[18t' > /dev/tty
+  IFS=';t' read -r _ rows cols _ < /dev/tty
+
+  stty "\$old"
+
+  # echo "cols:\$cols"
+  # echo "rows:\$rows"
+  stty cols "\$cols" rows "\$rows"
+}
+
+[ \$(tty) = /dev/ttyAMA0 ] && resize
