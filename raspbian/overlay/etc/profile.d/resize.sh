aa6ffe554535f592a6aeeb13ed116a1be4648d8a
Alistair Buxton <a.j.buxton@gmail.com>
2018-08-14 20:52:01 +0100

    resize.sh no longer needs to be escapes

--- raspbian/overlay/etc/profile.d/resize.sh
+++ raspbian/overlay/etc/profile.d/resize.sh
@@ -3,17 +3,17 @@
 
 resize() {
 
-  old=\$(stty -g)
+  old=$(stty -g)
   stty raw -echo min 0 time 5
 
   printf '\033[18t' > /dev/tty
   IFS=';t' read -r _ rows cols _ < /dev/tty
 
-  stty "\$old"
+  stty "$old"
 
   # echo "cols:\$cols"
   # echo "rows:\$rows"
-  stty cols "\$cols" rows "\$rows"
+  stty cols "$cols" rows "$rows"
 }
 
-[ \$(tty) = /dev/ttyAMA0 ] && resize
+[ $(tty) = /dev/ttyAMA0 ] && resize
